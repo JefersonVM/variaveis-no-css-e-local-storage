dbce877042f6e2eeec6591b9a37c870459e9a161
Jeferson Vieira <jeferson.vm1981@gmail.com>
2024-02-04 00:33:44 -0300

    Continuando com o conteúdo Variáveis no css e Local Storage

---
+++ exemplo02/js/script.js
@@ -0,0 +1,15 @@
+const root = document.querySelector(":root");
+const toggleButton = document.querySelector("button");
+
+toggleButton.addEventListener("click", () => {
+  const currentBgColor = root.style.getPropertyValue("--bg-color");
+
+  if (!currentBgColor || currentBgColor === "white") {
+    root.style.setProperty("--bg-color", "black");
+    root.style.setProperty("--text-color", "white");
+    return;
+  }
+
+  root.style.setProperty("--bg-color", "white");
+  root.style.setProperty("--text-color", "black");
+});
